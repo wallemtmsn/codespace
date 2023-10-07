dba6e756db7e1f6d121215e7436f5b609463dc3b
Wallemberg Nascimento <68344332+wallemtmsn@users.noreply.github.com>
2023-10-07 19:04:32 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = None
+    boas_vindas = "LOGIN EFETUADO COM SUCESSO!"
+    if request.method == "POST":
+        name = request.form['name']
+        password = request.form['password']
+        if name == "wallem" and password == "102030":
+            return redirect("/")
+        else:
+            error = "Login ou senha incorreto!"
+    return render_template("login.html", error=error)
